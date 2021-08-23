2dc94fbaaeb06226ed7cd503cd0a0d041c656482
xunwan <320626296@qq.com>
2021-08-23 19:25:45 +0800

    C2客户端更新操作

--- src/test/java/com/git/Test02.java
+++ src/test/java/com/git/Test02.java
@@ -3,6 +3,6 @@ package com.git;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("test......");
-        System.out.println("C1客户端操作test02...。");
+        System.out.println("C2客户端操作test02...。");
     }
 }
